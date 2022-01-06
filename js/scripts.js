b172efa895cd01e17886d16931759c80975f2059
João Castro <joao9pc@gmail.com>
2022-01-06 21:14:08 +0000

    Menus responsivos

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+/* Slide Show */
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -10,17 +12,29 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-var i;
-var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
-if (n > slides.length) {slideIndex = 1}    
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
 }
-for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+/* Menu Responsive */
+
+function myFunction() {
+    var x = document.getElementById("Menu");
+    if (x.className === "menu") {
+      x.className += " responsive";
+    } else {
+      x.className = "menu";
+    }
 }
-slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-}
+
