248e42e67162e12e5d597bf0cd8fbd250320c8f7
João Castro <joao9pc@gmail.com>
2022-03-19 19:46:28 +0000

    mais produtos adicionados

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";
 }
 
@@ -38,7 +38,6 @@ function myFunction() {
 
 /* Go Top */
 var mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() { scrollFunction() };
 
@@ -49,9 +48,15 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// Show the warning
+function noYetDone() {
+  var x = document.getElementById("warning");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
